c5961c94145d56d480affd8b9232b5403af924e3
Vectorized <webby1111@hotmail.com>
2022-12-08 02:27:48 +0000

    Update ClosedSea

--- test/helpers/Merkle.sol
+++ test/helpers/Merkle.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.4;
 import "./MurkyBase.sol";
 
 contract Merkle is MurkyBase {
-  function hashLeafPairs(bytes32 left, bytes32 right) public pure override returns (bytes32 _hash) {
-    assembly {
-      switch lt(left, right)
-        case 0 {
-          mstore(0x0, right)
-          mstore(0x20, left)
+    function hashLeafPairs(bytes32 left, bytes32 right) public pure override returns (bytes32 _hash) {
+        assembly {
+            switch lt(left, right)
+            case 0 {
+                mstore(0x0, right)
+                mstore(0x20, left)
+            }
+            default {
+                mstore(0x0, left)
+                mstore(0x20, right)
+            }
+            _hash := keccak256(0x0, 0x40)
         }
-        default {
-          mstore(0x0, left)
-          mstore(0x20, right)
-        }
-      _hash := keccak256(0x0, 0x40)
     }
-  }
 }
